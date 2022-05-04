63638735c6a5646d36df02602ccb9e25e80b687e
Avigail Ben Eliyahu <avigail.ben-eliyahu@ceva-dsp.com>
2022-05-04 18:24:15 +0300

    added cgal_sandbox.py and fixed init of add_dll

--- CGALPY_add_dlls/__init__.py
+++ CGALPY_add_dlls/__init__.py
@@ -1,6 +1,6 @@
 import os
-os.add_dll_directory(r"C:\RobotMotion\boost_1_79_0")
-os.add_dll_directory(r"C:\RobotMotion\gmp")
+os.add_dll_directory(r"C:\RobotMotion\boost_1_79_0\bin\x64\lib")
+os.add_dll_directory(r"C:\RobotMotion\gmp\lib")
 
 # os.add_dll_directory(<BOOST_LIBRARY_DIR>)
 # os.add_dll_directory(<GMP_LIBRARY_DIR>)
