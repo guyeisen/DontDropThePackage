d78a7cdfb8ddbb025d37bc83d4fd2e770c0e7e81
Avigail Ben Eliyahu <avigail.ben-eliyahu@ceva-dsp.com>
2022-05-04 16:42:33 +0300

    added package to import dlls

---
+++ CGALPY_add_dlls/__init__.py
@@ -0,0 +1,7 @@
+import os
+os.add_dll_directory(r"C:\RobotMotion\boost_1_79_0")
+os.add_dll_directory(r"C:\RobotMotion\gmp")
+
+# os.add_dll_directory(<BOOST_LIBRARY_DIR>)
+# os.add_dll_directory(<GMP_LIBRARY_DIR>)
+# os.add_dll_directory(<MPFR_LIBRARY_DIR>)
