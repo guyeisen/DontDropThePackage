d6dd19a20cc4960df7aba263a2f429d1e41d31a5
Avigail Ben Eliyahu <avigail.ben-eliyahu@ceva-dsp.com>
2022-05-27 18:02:58 +0300

    added all robomaster packages

--- CGALPY_add_dlls/__init__.py
+++ CGALPY_add_dlls/__init__.py
@@ -2,6 +2,7 @@ import os
 os.add_dll_directory(r"C:\RobotMotion\boost_1_79_0\bin\x64\lib")
 os.add_dll_directory(r"C:\RobotMotion\gmp\lib")
 
+
 # os.add_dll_directory(<BOOST_LIBRARY_DIR>)
 # os.add_dll_directory(<GMP_LIBRARY_DIR>)
 # os.add_dll_directory(<MPFR_LIBRARY_DIR>)
